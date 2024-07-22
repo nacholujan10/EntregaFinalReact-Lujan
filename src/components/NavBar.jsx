dd61d2a453f82a6ef2cfecfda0df30621f463bb6
Ignacio Lujan <nacholujan10@gmail.com>
2024-07-22 17:02:17 -0300

    primer commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,21 @@
+export const NavBar = () => {
+  return (
+    <>
+      <h6>Numeral Agro</h6>
+      <ul>
+        <li>
+          <a href="#">Inicio</a>
+        </li>
+        <li>
+          <a href="#">Contacto</a>
+        </li>
+        <li>
+          <a href="#">Quienes Somos</a>
+        </li>
+        <li>
+          <a href="#">Servicios</a>
+        </li>
+      </ul>
+    </>
+  );
+};
